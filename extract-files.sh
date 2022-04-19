a21cdae5a67238699aa9d3d81d9de204fd83936f
Adrian Lam <adrianlam230@gmail.com>
2022-04-19 10:53:35 +0800

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -58,4 +58,4 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" . "${KANG}" --section "${SECTION}"
 
-sudo "${MY_DIR}/setup-makefiles.sh"
+sudo bash "${MY_DIR}/setup-makefiles.sh"
