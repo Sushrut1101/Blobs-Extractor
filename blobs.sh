6e95b7df03bb3528b651554a0f961db516ad0aef
Adrian Lam <adrianlam230@gmail.com>
2022-04-19 11:17:39 +0800

    Update blobs.sh

--- blobs.sh
+++ blobs.sh
@@ -3,11 +3,16 @@
 mv *.sh *.txt ~/Dump/
 cd ~/Dump/
 bash extract-files.sh || echo "BRUH: check your extract-files.sh. Might be something wrong with it." && exit 1
-cd ../
-cd vendor/*/*
-git init && curl -s -X POST -H "Authorization: token ${GITHUB_TOKEN}" -d '{ "name": "'"${repo}"'"' "https://api.github.com/orgs/${GIT_ORG}/repos" } >/dev/null 2>&1
-git remote remove origin
-git remote add origin "https://github.com/"${GIT_ORG}"/"${repo}"
-git add --all
-git commit -asm "Blobs for umi"
+if [ -e ~/vendor/*/*/Android.bp ], then
+ cd ../
+ cd vendor/*/*
+ git init && curl -s -X POST -H "Authorization: token ${GITHUB_TOKEN}" -d '{ "name": "'"${repo}"'"' "https://api.github.com/orgs/${GIT_ORG}/repos" } >/dev/null 2>&1
+ git remote remove origin
+ git remote add origin "https://github.com/"${GIT_ORG}"/"${repo}"
+ git add --all
+ git commit -asm "Blobs for umi"
  echo "Blobs extraction done!"
+ exit 0
+else
+  echo "Blobs extraction failed" && exit 1
+fi
