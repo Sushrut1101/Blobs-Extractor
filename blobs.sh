c39951626aa016fb53ce1472f1aa8fabf7f53306
Adrian Lam <adrianlam230@gmail.com>
2022-04-19 13:50:46 +0800

    Update blobs.sh

--- blobs.sh
+++ blobs.sh
@@ -7,12 +7,7 @@ chmod 777 /root/Dump/*.sh
 bash extract-files.sh
 ls ../vendor/*/*
  echo "Blobs extraction done"
- cd ../
- cd vendor/*/*
- git init 
-	curl -s -X POST -H "Authorization: token ${GITHUB_TOKEN}" -d '{ "name": "'"${repo}"'", "description": "'"${description}"'"}' "https://api.github.com/orgs/${GIT_ORG}/repos" >/dev/null 2>&1
- git remote add origin "https://github.com/"${GIT_ORG}"/"${repo}"
- git add --all
- git commit -asm "Blobs for umi"
- git push origin HEAD:master -f
+ cd ..
+ cd vendor/*/
+ rclone copy drive:Blobs umi/
  exit 0
